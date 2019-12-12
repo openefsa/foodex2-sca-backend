1965c51f33e439b4b6e6bb872f5110f0adb96c3a
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2019-12-12 11:59:58 +0100

    new model creator based on the ComplementNB model able to predict the baseterm classification from a given free text

--- src/model_creator_2.py
+++ src/model_creator_2.py
@@ -1,29 +1,81 @@
-# Import all the dependencies
-from gensim.models.doc2vec import Doc2Vec
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from nltk.tokenize import word_tokenize
+# file used for create, train, save and test the model
 
-import logging
-logging.basicConfig(
-    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import pickle as pk
+import numpy as np
+
+''' IMPORT DATA '''
+# import the dataframe using pandas
+df = pd.read_csv('src/data/cleaned_history_3.csv')
+
+''' DROP NANs '''
+# remove rows having nan values
+print("shape w/ nan: ", df.shape)
+df.dropna(inplace=True)
+print("shape w/o nan: ", df.shape)
+
+''' DATA PREPARATION '''
+# get feature column
+X = df['ENFOODNAME']
+# get label column
+y = df['BASETERM_NAME']
+# split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01)
 
 
-raw_documents = open('./src/dataset/bt_dataset.txt').read().splitlines()
-tokenized_sents = [[w.lower() for w in word_tokenize(text)]
-            for text in raw_documents]
+def create_bow(X):
+    # create bag of words for the given series
+    print('Creating bag of words...')
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    # Initialize the TfidfVectorizer
+    vectorizer = TfidfVectorizer(ngram_range=(1, 2), use_idf=True)
+    # fit the model and transform training data into feature vectors
+    # (convert to a NumPy array for easy handling)
+    train_features = vectorizer.fit_transform(X)
+    # pickle the vocabulary
+    pk.dump(vectorizer, open("src/model/vectorizer.pkl", "wb"))
+    # words in vocabulary
+    # vocab = vectorizer.get_feature_names()
+    return vectorizer, train_features
 
-sentences = [TaggedDocument((doc), [i]) for i, doc in enumerate(tokenized_sents)]
 
-model = Doc2Vec(vector_size=5, window=2, dm=0, dbow_words=1, workers=4)
+# apply bag of words function to training set
+vectorizer, train_features = create_bow(X_train)
 
-model.build_vocab(sentences)
 
-for epoch in range(100):
-    print('iteration {0}'.format(epoch))
-    model.train(sentences, total_examples=model.corpus_count,
-                epochs=model.epochs)
-    model.alpha -= 0.0025  # decrease the learning rate
-    model.min_alpha = model.alpha  # fix the learning rate, no decay
+def train_model(features, label):
+    # CNB is an adaptation of the standard multinomial naive Bayes (MNB)
+    # it is particularly suited for imbalanced data sets
+    # since it uses statistics from the complement of each class to compute the model’s weights
 
-model.save("./src/model/d2v.model")
-print("Model Saved")
+    print("Training the Complement Naive Bayes model...")
+    from sklearn.naive_bayes import ComplementNB
+    ml_model = ComplementNB()
+    # ml_model.fit(features, label)
+    # partil_fit useful when dataset is too big to fit in memory at once
+    ml_model.partial_fit(features, label, classes=np.unique(label))
+    # print the model accuracy
+    score = ml_model.score(features, label) * 100
+    print('CNB Accuracy: %.0f%%'% score)
+    return ml_model
+
+
+# initialise and fit model
+ml_model = train_model(train_features, y_train)
+
+''' SAVE MODEL '''
+# save the model on disk
+pk.dump(ml_model, open('src/model/bt_model.pkl', 'wb'))
+
+''' TEST MODEL '''
+# vectorize the test data
+test_features = vectorizer.transform(X_test)
+
+# predict the code
+predicted_y = ml_model.predict(test_features)
+print("predicted values", predicted_y)
+correctly_identified_y = predicted_y == y_test
+print("correctly identified ", correctly_identified_y)
+accuracy = np.mean(correctly_identified_y) * 100
+print('Test accuracy: %.0f%%' % accuracy)
