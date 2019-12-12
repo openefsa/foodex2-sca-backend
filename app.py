5254de24350eba6972024b34c768ac58449b8feb
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2019-12-12 12:17:23 +0100

    many changes to the Flask interface

--- app.py
+++ app.py
@@ -1,55 +1,62 @@
 from flask import Flask, request
 from flask_cors import CORS
 
-from gensim.similarities import SparseMatrixSimilarity
-from gensim.corpora import Dictionary
-from gensim.models import TfidfModel
+from nltk import word_tokenize
 
-import numpy as np
-import pickle
+import pickle as pk
 
 # init Flask and enable Cross Origin Resource Sharing
 app = Flask(__name__)
 CORS(app)
 
-# load the baseterms
-file = open('src/dataset/pp_bt_dataset.pkl', 'rb')
-raw_doc = pickle.load(file)
-
-# load the model for the baseterm
-index_sparse = SparseMatrixSimilarity.load('src/model/baseterm.index')
-print('baseterm model loaded correctly')
-
-# load the dictionary for the baseterm
-dictionary = Dictionary.load('src/model/baseterm.dict')
-print('baseterm dictionary loaded correctly ', dictionary)
-
-# load tf-idf model fro the baseterm
-tf_idf = TfidfModel.load('src/model/baseterm.tfidf')
+# load the model from the external file
+ml_model = pk.load(open('src/model/bt_model.pkl', 'rb'))
+# load the vectorizer from the external file
+vectorizer = pk.load(open("src/model/vectorizer.pkl", "rb"))
 
 
-@app.route("/getSuggestions", methods=['POST'])
-def getSuggestions():
+def clean_text(raw_text):
+    # 1) Convert to lower case and Tokenize
+    tokens = word_tokenize(raw_text.lower())
+    # 2) Keep only words (removes punctuation and numbers)
+    tokens = [w for w in tokens if w.isalpha()]
+    # 3) Stemming
+    # tokens = [stemming.stem(w) for w in token_words]
+    # Return cleaned text
+    return " ".join(tokens)
+
+
+@app.route("/getBaseterm", methods=['POST'])
+def getBaseterm():
+    # get the passed json file
+    data = request.get_json()
+    # if key doesnt exist return none
+    baseterm = data['baseterm']
+    # pre process the inserted free text
+    cleaned_desc = [clean_text(baseterm)]
+
+    # vectorize the cleaned text with pre-built TfidfVectorizer (removes also stopwords)
+    test_data_features = vectorizer.transform(cleaned_desc)
+
+    # predict top best probabilities
+    probs = ml_model.predict_proba(test_data_features)
+
+    results = sorted(zip(ml_model.classes_, probs[0]), key=lambda x: x[1])[-10:]
+
+    # return as json
+    return results.to_json()
+
+'''
+@app.route("/getFacets", methods=['POST'])
+def getFacets():
     # get the passed json file
     data = request.get_json()
 
-    # if key doesnt exist return none
-    baseterm = data['baseterm']
-    # facets = data['facets']
+    # retrieve the selected baseterm index
+    btIndex = int(data['btIndex'])
 
-    query_doc_bow = dictionary.doc2bow(baseterm)
-    print(query_doc_bow)
-    query_doc_tf_idf = tf_idf[query_doc_bow]
-    print(query_doc_tf_idf)
-
-    # get distance for each sentence in doc (top 10)
-    sim = index_sparse[query_doc_tf_idf]
-    # save the list of indexes
-    indexes = [i[0] for i in sim]
-
-    # get best results
-    return ','.join([raw_doc[x] for x in indexes])
-
+    return ""
+'''
 
 if __name__ == "__main__":
     app.run(debug=True)
