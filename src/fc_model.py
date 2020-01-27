933202244d57d215b87e1154c5f005f4ba30d5e3
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-01-27 11:13:18 +0100

    code optimisation

--- src/fc_model.py
+++ src/fc_model.py
@@ -7,37 +7,40 @@ import numpy as np
 
 ''' IMPORT DATA '''
 # import the dataframe using pandas
-df = pd.read_csv('src/data/FoodEx2_History_Facets.csv')
+df = pd.read_csv('src/data/fc_data_model.csv')
+df = df.sample(frac=0.2)
+
+print(df.shape)
 
 ''' DROP NANs '''
 # remove rows having nan values
 print("shape w/ nan: ", df.shape)
-df = df.fillna('')
-print("shape w/o nan: ", df.shape)
+# df.dropna(inplace=True)
+df.fillna('', inplace=True)
 
-# use 50% of the whole dataframe (random_state=1 used for reproducing the same sample)
-df = df.sample(frac=0.05)
+print("shape w/o nan: ", df.shape)
 
 ''' DATA PREPARATION '''
 # get feature column
-X = df['BASETERM_NAME']
+X = df["BT_DESC"]
 # get label column
-y = df.drop('BASETERM_NAME', axis=1)
+y = df['EXPLICIT']
 # split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01)
 
 
 def create_bow(X):
     # create bag of words for the given series
-    print('Creating bag of words...')
+    print("Creating bag of words...")
     from sklearn.feature_extraction.text import TfidfVectorizer
+
     # Initialize the TfidfVectorizer
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2), use_idf=True)
+    vectorizer = TfidfVectorizer(ngram_range=(1, 1), use_idf=True)
     # fit the model and transform training data into feature vectors
     # (convert to a NumPy array for easy handling)
     train_features = vectorizer.fit_transform(X)
     # pickle the vocabulary
-    pk.dump(vectorizer, open("src/model/vectorizer.pkl", "wb"))
+    pk.dump(vectorizer, open("src/model/fc_vectorizer.pkl", "wb"))
     # words in vocabulary
     # vocab = vectorizer.get_feature_names()
     return vectorizer, train_features
@@ -48,21 +51,26 @@ vectorizer, train_features = create_bow(X_train)
 
 
 def train_model(features, label):
-    
-    print("Training the Complement Naive Bayes model...")
     # CNB is an adaptation of the standard multinomial naive Bayes (MNB)
     # it is particularly suited for imbalanced data sets
     # since it uses statistics from the complement of each class to compute the model’s weights
-    from sklearn.multioutput import MultiOutputClassifier
-    from sklearn.neighbors import KNeighborsClassifier
-    ml_model = MultiOutputClassifier(KNeighborsClassifier())
-    ml_model.fit(features, label)
+
+    print("Training the Complement Naive Bayes model...")
+    
+    from sklearn.naive_bayes import ComplementNB
+    ml_model = ComplementNB()
+    # ml_model.fit(features, label)
     # partil_fit useful when dataset is too big to fit in memory at once
-    #ml_model.partial_fit(features, label, classes=np.unique(label))
+    ml_model.partial_fit(features, label, classes=np.unique(label))
     # print the model accuracy
     score = ml_model.score(features, label) * 100
-    print('CNB Accuracy: %.0f%%' % score)
-
+    print('CNB Accuracy: %.0f%%'% score)
+    '''
+    from sklearn.neural_network import MLPClassifier
+    ml_model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    
+    ml_model.fit(features, label)
+    '''
     return ml_model
 
 
@@ -75,19 +83,13 @@ pk.dump(ml_model, open('src/model/fc_model.pkl', 'wb'))
 
 ''' TEST MODEL '''
 # vectorize the test data
-''' TEST MODEL '''
-# vectorize the test data
-test_features = vectorizer.transform(["white chocolate with hazelnuts"])
+print(X_test)
+test_features = vectorizer.transform(X_test)
 
-## predict the code
+# predict the code
 predicted_y = ml_model.predict(test_features)
-
-# print result
 print("predicted values", predicted_y)
-
-
-## predict the code
-predicted_y = ml_model.predict(test_features)
-
-# print result
-print("predicted values", predicted_y)
+correctly_identified_y = predicted_y == y_test
+print("correctly identified ", correctly_identified_y)
+accuracy = np.mean(correctly_identified_y) * 100
+print('Test accuracy: %.0f%%' % accuracy)
