361cf34551f09184e55a80351055e7c4174b33f9
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-07-20 13:14:12 +0200

    added k8s python client, added azure table service, api authorisation based secrets,

--- api/private.py
+++ api/private.py
@@ -2,22 +2,21 @@
 # -*- coding:utf-8 -*-
 ###
 # *********************************************************************
-# |                                                                    
+# |
 # | File: \private.py
 # | Description: <<desc here>>
 # | Project: api
 # | Created Date: 19th May 2020
 # | Author: Alban Shahaj (shahaal)
 # | Email: data.collection@efsa.europa.eu
-# | -----------------------------------------------------------------  
+# | -----------------------------------------------------------------
 # | Last Modified: Thursday, 24th June 2020
 # | Modified By: Alban Shahaj (shahaal)
-# | -----------------------------------------------------------------  
+# | -----------------------------------------------------------------
 # | Copyright (c) 2020 European Food Safety Authority (EFSA)
-# |                                                                    
+# |
 # *********************************************************************
 ###
-
 
 
 from flask import request
@@ -32,21 +31,35 @@ import json
 from azure.cosmosdb.table.tableservice import TableService
 from azure.cosmosdb.table.models import Entity
 
-from config import Config
+from kubernetes import client, config
+
+# load k8s configuration
+try:
+    config.load_incluster_config()
+except config.ConfigException:
+    try:
+        config.load_kube_config()
+    except config.ConfigException:
+        raise Exception("Could not configure kubernetes python client")
+
+v1 = client.CoreV1Api()
+# pull secrets from cred in deafult namespace
+secret = v1.read_namespaced_secret("creds", "default")
+print(secret)
 
 # initiate the table service
 table_service = TableService(
-    account_name=Config.ACCOUNT_NAME, 
-    account_key=Config.ACCOUNT_KEY
+    account_name=secret.BLOB_STORAGE_ACCOUNT_NAME,
+    account_key=secret.BLOB_STORAGE_ACCOUNT_KEY
 )
 
 # if the table does not exsits than create it
-tableName = Config.AZURE_TABLE_NAME
+tableName = secret.AZURE_TABLE_NAME
 if not table_service.exists(tableName):
     table_service.create_table(tableName)
 
 
-def sharedkey_required(f):
+def authorisation_required(f):
     ''' method used to check if the user has permission to post feedbacks '''
 
     @wraps(f)
@@ -54,16 +67,17 @@ def sharedkey_required(f):
         key = None
 
         if 'x-access-token' in request.headers:
-            key = request.headers['x-access-token']
-            enable_feedbacks = (key == Config.SECRET_KEY)
+            token = request.headers['x-access-token']
+            # enable feedbacks if status code ok
+            enable_feedbacks = (token == secret.FEEDBACK_API_SECRET_KEY)
 
         return f(enable_feedbacks, *args, **kwargs)
 
     return decorated
-    
+
 
 @api.route('/postFeedback', methods=['POST'])
-@sharedkey_required
+@authorisation_required
 def postFeedback(enable_feedbacks):
     # if current user is active
     if enable_feedbacks:
@@ -81,4 +95,3 @@ def postFeedback(enable_feedbacks):
         return json.dumps({'message': 'Feedback sent correctly'})
     else:
         return json.dumps({'message': 'An error occured while sending the feedback please try the administrator.'})
-
