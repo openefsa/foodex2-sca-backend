5f0b159a1cba9af3c1dc23f6f926861c7fbab802
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-11-11 11:41:02 +0100

    optimisation on using single cursor for private and public api

--- api/__init__.py
+++ api/__init__.py
@@ -18,10 +18,17 @@
 # *********************************************************************
 ###
 
+import pathlib, os, sqlite3 as sq
 
 from flask import Blueprint
 
 api = Blueprint('api', __name__, template_folder='templates')
+
+# create connection with local db
+db_path = pathlib.Path(os.path.abspath('api/asset/data/MTX_11.2.db')).as_uri()
+conn = sq.connect('{}?mode=ro'.format(db_path), uri=True, check_same_thread=False)
+# create cursor that will operate on db
+c = conn.cursor()
 
 # private and public apis must be imported after Blueprint initialisation
 from . import private, public
